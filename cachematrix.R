47837be25fc3dc9c3b7c815c487cdf7cd4a278f2
equj <30753904+equj@users.noreply.github.com>
2017-09-18 22:26:41 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates an object "matrix" which is a list that contains
+## functions set, get, setinverse and get invers.
+## Once called with an argument of numeric matrix, it creates a list with 
+## functions that have this numeric matrix as an argument
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+
+## cacheSolve takes the "matrix" object created by the function above
+## and checks whether this object was used to calculate inverse. If it did, than
+## this function just takes outputs the cached value. If not - it calculates the inverse
+## the matrix again.
+
+cacheSolve <- function(x, ...)  {
+  m <- x$getinverse()
+  
+  if(!is.null(m)) {
+    message("getting cached inverse")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
